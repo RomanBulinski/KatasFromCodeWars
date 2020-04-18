cdd4446dc1eca9faa52e53e2938ba60889cbf8a4
roman <aboandrus@gmail.com>
2020-04-18 20:00:37 +0200

    Start project

---
+++ src/main/java/ManyLanguages.java
@@ -0,0 +1,12 @@
+import java.util.List;
+import java.util.Map;
+
+public class ManyLanguages {
+
+    public static List<String> myLanguages(final Map<String, Integer> results) {
+        return null;
+    }
+
+
+
+}
