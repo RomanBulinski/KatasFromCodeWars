9af6bc5618030a6f86acff4d1c37454c8d6782fd
roman <aboandrus@gmail.com>
2020-04-19 17:01:58 +0200

    Refactor code for BasicCalculator kata

--- src/main/java/BasicCalculator.java
+++ src/main/java/BasicCalculator.java
@@ -1,32 +1,12 @@
 public class BasicCalculator {
 
     public static Double calculate(final double numberOne, final String operation, final double numberTwo) {
-        Double result = null;
-
         switch (operation) {
-            case "+":
-                result = numberOne + numberTwo;
-                break;
-            case "-":
-                result = numberOne - numberTwo;
-                break;
-            case "*":
-                if ( numberOne == 0D || numberTwo == 0D) {
-                    result = 0D;
-                } else {
-                    result = numberOne * numberTwo;
-                }
-                break;
-            case "/":
-                if (numberTwo == 0) {
-                    result = null;
-                    break;
-                }
-                result = numberOne / numberTwo;
-                break;
-            default:
+            case "+": return numberOne + numberTwo;
+            case "-": return numberOne - numberTwo;
+            case "*": return numberOne == 0D || numberTwo == 0D ? 0D : numberOne * numberTwo;
+            case "/": return numberTwo == 0 ?  null : numberOne / numberTwo;
         }
-        return result;
-
+        return null;
     }
 }
