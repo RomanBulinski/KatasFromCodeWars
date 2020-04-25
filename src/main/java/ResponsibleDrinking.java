b2ca14ff2b982b565c34d5619b74d00cd10115a9
roman <aboandrus@gmail.com>
2020-04-25 20:27:35 +0200

    Implemented code for ResponsibleDrinking kata

--- src/main/java/ResponsibleDrinking.java
+++ src/main/java/ResponsibleDrinking.java
@@ -1,4 +1,9 @@
+
+
+import com.sun.xml.internal.ws.util.StringUtils;
+
 import java.util.Arrays;
+import java.util.stream.Collectors;
 
 public class ResponsibleDrinking {
 
@@ -6,14 +11,14 @@ public class ResponsibleDrinking {
 //    "1 shot, 5 beers, 2 shots, 1 glass of wine, 1 beer"
 
     public String hydrate(String drinkString) {
-
-        Long xss = Arrays.asList( drinkString.split(" ") )
+        Integer glassesNumber = Arrays.asList( drinkString.split(" ") )
                 .stream()
-                .filter(String::isInstance)
-                .map (::cast)
-                .filter( x ->  x instanceof Integer )
-                .count();
-        // Insert party here
-        return "";
+                .filter( x -> x.matches("-?\\d+(\\.\\d+)?") )
+                .mapToInt(Integer::valueOf)
+                .sum();
+        return glassesNumber.equals( new Integer(1))? "1 glass of water" : glassesNumber + " glasses of water";
     }
+
+
+
 }
