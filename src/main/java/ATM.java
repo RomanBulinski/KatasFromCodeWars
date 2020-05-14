7455004844aaaf0240dab343ab72fdb87c48c505
roman <aboandrus@gmail.com>
2020-05-14 18:01:47 +0200

    Implemented Kata ATM

--- src/main/java/ATM.java
+++ src/main/java/ATM.java
@@ -1,9 +1,28 @@
 public class ATM {
 
+
+//    10, 20, 50, 100, 200 and 500 dollars.
+//    1<=n<=1500.
+
     public int solve(int n) {
 
-        // Your code here
+        int counter = 0;
+        int posistion = 0;
+        int[] dividers = {500, 200, 100, 50, 20, 10};
 
+        if (n % 10 != 0) {
+            return -1;
+        }
+        while (n > 0 ) {
+            while (n - dividers[posistion] >= 0) {
+                n = n - dividers[posistion];
+                counter++;
+            }
+            if(posistion<5){
+                posistion++;
+            }
+        }
+        return counter;
     }
 
 }
