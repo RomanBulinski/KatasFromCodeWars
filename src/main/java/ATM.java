f181ea164d07a131d5b031c89d9ac7c79ababdee
roman <aboandrus@gmail.com>
2020-05-14 18:11:43 +0200

    Code refactor in  Kata ATM

--- src/main/java/ATM.java
+++ src/main/java/ATM.java
@@ -1,26 +1,20 @@
 public class ATM {
 
-
-//    10, 20, 50, 100, 200 and 500 dollars.
-//    1<=n<=1500.
-
     public int solve(int n) {
 
         int counter = 0;
-        int posistion = 0;
+        int position = 0;
         int[] dividers = {500, 200, 100, 50, 20, 10};
 
         if (n % 10 != 0) {
             return -1;
         }
         while (n > 0 ) {
-            while (n - dividers[posistion] >= 0) {
-                n = n - dividers[posistion];
+            while (n - dividers[position] >= 0) {
+                n = n - dividers[position];
                 counter++;
             }
-            if(posistion<5){
-                posistion++;
-            }
+            if(position<5) position++;
         }
         return counter;
     }
