174c589dfe8c64e325311b94b76ada4f94467cff
roman <aboandrus@gmail.com>
2020-05-16 09:33:38 +0200

    Implemented code for Kata ReturnTheFirstMmultiplesOfN

--- src/main/java/ReturnTheFirstMmultiplesOfN.java
+++ src/main/java/ReturnTheFirstMmultiplesOfN.java
@@ -1,7 +1,11 @@
 public class ReturnTheFirstMmultiplesOfN {
 
     public static int[] multiples(int m, int n) {
-        // Code here!
+        int[] result = new int[m];
+        for(int i =0; i<m; i++){
+            result[i] = n * (i+1);
+        }
+        return result;
     }
 
 }
